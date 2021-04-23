a91f724334cadc8636827e88737b6728d02cbda3
asceznyk <asceznyk@gmail.com>
2021-04-23 22:10:04 +0530

    imports from augmentations ..

--- datasets.py
+++ datasets.py
@@ -11,6 +11,8 @@ from torch.utils.data import Dataset, DataLoader
 
 from torchvision import transforms
 
+from augmentations import *
+
 class ImageFolder(Dataset):
     def __init__(self, fpath, transform=None):
         self.files = [p for p in sorted(glob.glob("%s/*.*" % fpath)) if p[-4:] == '.jpg']
