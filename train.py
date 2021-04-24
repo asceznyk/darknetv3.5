685cd647cef20fa8fc02f9fff6d4e6b5a3d8b586
asceznyk <asceznyk@gmail.com>
2021-04-24 14:16:37 +0530

    bound code to a train function

--- train.py
+++ train.py
@@ -24,116 +24,109 @@ from models import *
 from losses import *
 from datasets import *
 
-'''
-cfgpath = options['cfg']
-weightspath = 'darknet53.conv.74'
-customwpath = rootpath+'customwyolov3.pth'
-traindir, validdir = 'train/', 'valid/'
-pcolor = (255, 0, 0)
-
-##hyperparameters
-epochs = 100
-batchsize = 4
-accumgradient = 2
-ncpu = 2
-imgsize = 416
-ckptinterval = 2
-
-names = get_names('obj.names')
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = Darknet(cfgpath, imgwh=imgsize).to(device)
-model.apply(init_normal)
+def train_darknet(options):
+    cfgpath = options.cfg
+    ptptweightspath = options.ptweightspth
+    ckptpth = options.ckptpth
+    traindir, validdir = options.traindir, options.validdir
+    pcolor = (255, 0, 0)
 
-print(model)
+    ##hyperparameters
+    epochs = options.epochs
+    batchsize = options.batchsize
+    accumgradient = 2
+    ncpu = options.ncpu
+    imgsize = 416
+    ckptinterval = 2
+ 
+    model = Darknet(cfgpath, imgwh=imgsize).to(device)
+    model.apply(init_normal)
 
-if weightspath.endswith('.pth'):
-    model.load_state_dict(torch.load(weightspath))
-else:
-    model.load_weights(weightspath)
+    print(model)
 
-traindata = ListDataset(traindir, imgsize, multiscale=True, transform=AUGMENTATIONTRANSFORMS)
-validdata = ListDataset(validdir, imgsize, multiscale=False, transform=DEFAULTTRANSFORMS)
+    if ptweightspath.endswith('.pth'):
+        model.load_state_dict(torch.load(ptweightspath))
+    else:
+        model.load_weights(ptweightspath)
 
-trainloader = DataLoader(traindata, batchsize, shuffle=True,
-                         num_workers=ncpu, pin_memory=True, 
-                         collate_fn=traindata.collate_fn)
+    traindata = ListDataset(traindir, imgsize, multiscale=True, transform=AUGMENTATIONTRANSFORMS)
+    validdata = ListDataset(validdir, imgsize, multiscale=False, transform=DEFAULTTRANSFORMS)
 
-validloader = DataLoader(validdata, batchsize, shuffle=False,
-                         num_workers=1, pin_memory=True, 
-                         collate_fn=validdata.collate_fn)
+    trainloader = DataLoader(traindata, batchsize, shuffle=True,
+                             num_workers=ncpu, pin_memory=True,
+                             collate_fn=traindata.collate_fn)
 
-optimizer = torch.optim.Adam(
-            model.parameters(),
-            )
+    validloader = DataLoader(validdata, batchsize, shuffle=False,
+                             num_workers=1, pin_memory=True,
+                             collate_fn=validdata.collate_fn)
 
-criterion = ComputeLoss(model, IoULoss)
+    optimizer = torch.optim.Adam(model.parameters())
 
-bestloss = 1e+5
-patience = 1e+5
-orgpatience = patience
-for e in range(epochs):
+    criterion = ComputeLoss(model, IoULoss)
 
-    model.train()
-    epochloss = 0
-    for b, (_, imgs, targets) in enumerate(trainloader):
-        batchesdone = len(trainloader) * e + b
-        imgs = Variable(imgs.to(device))
-        targets = Variable(targets.to(device), requires_grad=False)
+    bestloss = 1e+5
+    patience = 1e+5
+    orgpatience = patience
+    for e in range(epochs):
 
-        outputs = model(imgs, 'train')
-        loss = criterion(outputs, targets) 
-
-        loss.backward()
-
-        if  batchesdone % accumgradient == 0:
-            optimizer.step()
-            optimizer.zero_grad()
-        
-        batchloss = to_cpu(loss).item()
-        epochloss += batchloss
-
-        print(f'training loss at batch {b}: {batchloss:.3f}')
-
-    epochloss /= len(trainloader)
-
-    print(f'training loss at epoch {e}: {epochloss:.3f}')
-
-    model.eval()
-    epochloss = 0
-    for b, (_, imgs, targets) in enumerate(validloader):
-        with torch.no_grad():
-            imgs = Variable(imgs.to(device), requires_grad=False)
+        model.train()
+        epochloss = 0
+        for b, (_, imgs, targets) in enumerate(trainloader):
+            batchesdone = len(trainloader) * e + b
+            imgs = Variable(imgs.to(device))
             targets = Variable(targets.to(device), requires_grad=False)
 
             outputs = model(imgs, 'train')
-            loss = criterion(outputs, targets)
+            loss = criterion(outputs, targets) 
+
+            loss.backward()
+
+            if  batchesdone % accumgradient == 0:
+                optimizer.step()
+                optimizer.zero_grad()
             
             batchloss = to_cpu(loss).item()
             epochloss += batchloss
 
-        print(f'validation loss at batch {b}: {batchloss:.3f}')
+            print(f'training loss at batch {b}: {batchloss:.3f}')
 
-    epochloss /= len(validloader)
+        epochloss /= len(trainloader)
 
-    print(f'validation loss at epoch {e}: {epochloss:.3f}')
+        print(f'training loss at epoch {e}: {epochloss:.3f}')
 
-    patience -= 1
-    if epochloss <= bestloss:
-        torch.save(model.state_dict(), customwpath)
-        bestloss = epochloss 
-        patience = orgpatience
+        model.eval()
+        epochloss = 0
+        for b, (_, imgs, targets) in enumerate(validloader):
+            with torch.no_grad():
+                imgs = Variable(imgs.to(device), requires_grad=False)
+                targets = Variable(targets.to(device), requires_grad=False)
 
-        print(f'saved best model weights at epoch {e} to {customwpath}')
+                outputs = model(imgs, 'train')
+                loss = criterion(outputs, targets)
+                
+                batchloss = to_cpu(loss).item()
+                epochloss += batchloss
 
-    if not patience:
-        print(f'early stopping.. validation loss did not improve from {bestloss:.3f}')
-        print(f'you can change the patience value... current value {orgpatience} epochs')
-        break
+            print(f'validation loss at batch {b}: {batchloss:.3f}')
 
-model.load_state_dict(torch.load(customwpath))
-'''
+        epochloss /= len(validloader)
+
+        print(f'validation loss at epoch {e}: {epochloss:.3f}')
+
+        patience -= 1
+        if epochloss <= bestloss:
+            torch.save(model.state_dict(), customwpath)
+            bestloss = epochloss
+            patience = orgpatience
+
+            print(f'saved best model weights at epoch {e} to {customwpath}')
+
+        if not patience:
+            print(f'early stopping.. validation loss did not improve from {bestloss:.3f}')
+            print(f'you can change the patience value... current value {orgpatience} epochs')
+            break
 
 def test_func(options):
     print(options.traindir, options.validdir)
@@ -142,13 +135,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--traindir', type=str, help='path to training set')
     parser.add_argument('--validdir', type=str, help='path to validation set')
-    parser.add_argument('--names', type=str, help='path to file which contains all class names')
     parser.add_argument('--cfg', type=str, help='a .cfg file for model architecture')
     parser.add_argument('--ptweightspth', type=str, help='path to pretrained weights')
     parser.add_argument('--ckptpth', type=str, help='path to save trained model')
-    parser.add_argument('--epochs', type=int)
-    parser.add_argument('--batchsize', type=int)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--batchsize', type=int, default=4)
+    parser.add_argument('--ncpu', type=int, default=2)
 
     options = parser.parse_args()
 
-    test_func(options)
+    train_darknet(options)
