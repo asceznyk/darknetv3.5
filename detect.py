1877525f27b409d5669ea65fdd703c7a367e6811
asceznyk <asceznyk@gmail.com>
2021-04-24 16:08:17 +0530

    added pre-trained weights duh!

--- detect.py
+++ detect.py
@@ -26,6 +26,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 def detect_darknet(options):
     imgsize = 416
     model = Darknet(options.cfg, imgwh=imgsize).to(device)
+    model.load_state_dict(torch.load(options.weights))
 
     print('showing the actual boxes...')
     show_boxes(options.names, options.testdir, imgsize, options.savedir)
@@ -38,6 +39,7 @@ if __name__ == '__main__':
     parser.add_argument('--testdir', type=str, help='path to image folder')
     parser.add_argument('--names', type=str, help='path to names file')
     parser.add_argument('--cfg', type=str, help='a .cfg file for model architecture')
+    parser.add_argument('--weights', type=str, help='path to pre-trained weights')
     parser.add_argument('--savedir', type=str, help='folder to save detected images')
 
     options = parser.parse_args()
