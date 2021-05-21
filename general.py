f24d7b34b81260bc2effe9fc89fdf27aa127f314
asceznyk <asceznyk@gmail.com>
2021-05-21 14:17:59 +0530

    sorry about that!

--- general.py
+++ general.py
@@ -254,4 +254,4 @@ def one_cycle(y1=0.0, y2=1.0, steps=100):
     # lambda function for sinusoidal ramp from y1 to y2
     return lambda x: ((1 - math.cos(x * math.pi / steps)) / 2) * (y2 - y1) + y1
 
-def mean_ap():
+#def mean_ap():
