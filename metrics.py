dd97005faf82a62e2973534880bde3b350e38950
asceznyk <asceznyk@gmail.com>
2021-05-29 12:32:06 +0530

    first time map done

--- metrics.py
+++ metrics.py
@@ -59,12 +59,6 @@ def mean_ap(predictions, targets, nclasses, iouthresh=0.5):
             if gtbox[1] == c:
                 groundtruths.append(gtbox.tolist())
 
-        print('groundtruths: ', groundtruths)
-        print('---')
-        print('detections: ', detections)
-
-        print([gt[0] for gt in groundtruths])
-
         #countgtboxs = Counter([int(gt[0]) for gt in groundtruths])
         countgtboxs = {}
         for gtbox in groundtruths:
@@ -74,16 +68,45 @@ def mean_ap(predictions, targets, nclasses, iouthresh=0.5):
             else:
                 countgtboxs[idx] += 1
 
-        print(countgtboxs)
+        #print(countgtboxs)
         for k, v in countgtboxs.items():
             countgtboxs[k] = torch.zeros(v)
 
         detections.sort(key=lambda x: x[2], reverse=True)
+        TP = torch.zeros(len(detections))
+        FP = torch.zeros(len(detections))
+        totalgts = len(groundtruths)
 
-        print(detections)
-        print(countgtboxs)
+        if totalgts == 0:
+            continue
 
-        #for i, detection in enumerate(detections):
+        for i, detection in enumerate(detections):
+            imggts = [gt for gt in groundtruths if gt[0] == detection[0]]
+            bestiou = 0
+            for j, gtbox in enumerate(imggts):
+                iou = bbox_iou(torch.tensor(detection[3:]), torch.tensor(gtbox[3:]))
 
+                if iou > bestiou:
+                    bestiou = iou
+                    bestgtidx = j
+
+            if bestiou > iouthresh:
+                if countgtboxs[detection[0]][bestgtidx] == 0:
+                    TP[i] = 1
+                    countgtboxs[detection[0]][bestgtidx] = 1
+                else:
+                    FP[i] = 1
+            else:
+                FP[i] = 1
+
+        TPcumsum = torch.cumsum(TP, dim=0)
+        FPcumsum = torch.cumsum(FP, dim=0)
+        recalls = TPcumsum / (totalgts + epsilon)
+        precisions = TPcumsum / (TPcumsum + FPcumsum + epsilon)
+        recalls = torch.cat(torch.tensor([0]), recalls)
+        precisions = torch.cat(torch.tensors[1], precisions)
+        aps.append(torch.trapz(precisions, recalls))
+
+    return sum(aps) / len(aps)
 
 
